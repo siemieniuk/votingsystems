9b5ff000ac209b3cb01c212a19ec9371f0ba75e1
Szymon Siemieniuk <36564363+siemieniuk@users.noreply.github.com>
2024-06-21 19:48:07 +0200

    Implemented isConsistent() methods

--- src/main/java/io/github/siemieniuk/votingsystems/ballot/dataset/ScoreBallotDataset.java
+++ src/main/java/io/github/siemieniuk/votingsystems/ballot/dataset/ScoreBallotDataset.java
@@ -2,6 +2,7 @@ package io.github.siemieniuk.votingsystems.ballot.dataset;
 
 import io.github.siemieniuk.votingsystems.ballot.ScoreBallot;
 import io.github.siemieniuk.votingsystems.ballot.entry.CandidateEntry;
+import io.github.siemieniuk.votingsystems.ballot.entry.EntryWithNumber;
 import io.github.siemieniuk.votingsystems.ballot.entry.ScoreBallotEntry;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,5 +30,19 @@ public class ScoreBallotDataset
                 .stream()
                 .map(ScoreBallotEntry::getPreference)
                 .toList());
+    }
+
+    @Override
+    public boolean isConsistent() {
+        for (ScoreBallot ballot : getBallots()) {
+            List<CandidateEntry> candidatesInBallot = ballot.getPreferences()
+                    .stream()
+                    .map(EntryWithNumber::getPreference)
+                    .toList();
+            if (!getCandidates().containsAll(candidatesInBallot)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
